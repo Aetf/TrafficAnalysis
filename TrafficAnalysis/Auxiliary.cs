b39df4a19bd0b70c9eb50cc21e7034502d72dd81
Aetf <7437103@gmail.com>
2013-08-02 19:32:44 +0800

    添加工具类

---
+++ TrafficAnalysis/Auxiliary.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TrafficAnalysis.Util
+{
+    public static class Auxiliary
+    {
+        public static double Height(this Thickness thickness)
+        {
+            return thickness.Top + thickness.Bottom;
+        }
+
+        public static double Width(this Thickness thickness)
+        {
+            return thickness.Left + thickness.Right;
+        }
+    }
+}
