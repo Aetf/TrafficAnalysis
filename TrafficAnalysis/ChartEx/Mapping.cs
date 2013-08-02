dbc37f5e718909ad617e7accc1de6dea5cb50422
Aetf <7437103@gmail.com>
2013-08-02 22:49:13 +0800

    添加了适用于DynamicDataDisplay的DataSource，支持删除操作

---
+++ TrafficAnalysis/ChartEx/Mapping.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows;
+
+namespace TrafficAnalysis.ChartEx
+{
+    class Mapping<TSource>
+    {
+        internal DependencyProperty Property { get; set; }
+        internal Func<TSource, object> F { get; set; }
+    }
+}
