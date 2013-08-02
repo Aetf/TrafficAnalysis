4800d5e6365eb0b11d4bb25bf7342e0cdf0391e6
Aetf <7437103@gmail.com>
2013-08-02 22:49:54 +0800

    添加WinPcap封装库的统一抽象接口

---
+++ TrafficAnalysis/DeviceDataSource/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficAnalysis.DeviceDataSource
+{
+    /// <summary>
+    /// Represents a abstruct interface's basic informations.
+    /// </summary>
+    public class DeviceDes
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FriendlyName { get; set; }
+    }
+}
---
+++ TrafficAnalysis/DeviceDataSource/IStatisticsSource.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficAnalysis.DeviceDataSource
+{
+    /// <summary>
+    /// Represent statistics information for an interface.
+    /// </summary>
+    public struct StatisticsInfo
+    {
+        /// <summary>
+        /// Bit per second
+        /// </summary>
+        public double Bps;
+
+        /// <summary>
+        /// Packet per second
+        /// </summary>
+        public double Pps;
+
+        /// <summary>
+        /// Packer per second classified by network layer
+        /// </summary>
+        public Dictionary<string, long> NetworkLayer;
+
+        /// <summary>
+        /// Packet per second classified by transport layer packet type
+        /// </summary>
+        public Dictionary<string, long> TransportLayer;
+
+        /// <summary>
+        /// Packet per second classified by application layer packet type
+        /// </summary>
+        public Dictionary<string, long> ApplicationLayer;
+
+        public StatisticsInfo(ulong bps, ulong pps)
+        {
+            Bps = bps;
+            Pps = pps;
+            NetworkLayer = new Dictionary<string, long>();
+            TransportLayer = new Dictionary<string, long>();
+            ApplicationLayer = new Dictionary<string, long>();
+        }
+    }
+
+    /// <summary>
+    /// Interface for an abstract warpper around WinPcap
+    /// </summary>
+    interface IStatisticsSource
+    {
+        Dictionary<string, StatisticsInfo> Statistics { get; }
+        ReadOnlyCollection<DeviceDes> DeviceList { get; }
+
+        void StartCapture(DeviceDes des);
+        void StopCapture(DeviceDes des);
+    }
+}
