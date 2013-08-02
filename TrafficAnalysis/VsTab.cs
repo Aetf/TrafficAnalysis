b9a3b0d9d6a65aa779b392a4bc54c3e9942c082a
Aetf <7437103@gmail.com>
2013-08-02 19:33:46 +0800

    实现Vs样式的TabControl

---
+++ TrafficAnalysis/VsTab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TrafficAnalysis
+{
+    static public class ControlEx
+    {
+        public static readonly DependencyProperty IconProperty =
+          DependencyProperty.RegisterAttached("Icon", typeof(ImageSource), typeof(ControlEx), new PropertyMetadata(default(ImageSource)));
+
+        public static void SetIcon(UIElement element, ImageSource value)
+        {
+            element.SetValue(IconProperty, value);
+        }
+
+        public static ImageSource GetIcon(UIElement element)
+        {
+            return (ImageSource)element.GetValue(IconProperty);
+        }
+    }
+
+    class VsTab
+    {
+    }
+}
