d60e2fa236f06cab48688c1562176bbf8ae7d051
Aetf <7437103@gmail.com>
2013-08-26 00:50:16 +0800

    完成新建捕获任务-详细的对话框

--- TrafficAnalysis/StartNewCaptureDetial.xaml.cs
+++ TrafficAnalysis/StartNewCaptureDetial.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PcapDotNet.Core;
+using TrafficAnalysis.DeviceDataSource;
 
 namespace TrafficAnalysis
 {
@@ -19,9 +21,53 @@ namespace TrafficAnalysis
     /// </summary>
     public partial class StartNewCaptureDetial : Window
     {
-        public StartNewCaptureDetial()
+        DeviceDes des;
+
+        public StartNewCaptureDetial(DeviceDes des)
         {
+            this.des = des;
             InitializeComponent();
+        }
+
+        public DumpOptions Options { get; private set; }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan durance = TimeSpan.MaxValue;
+            if (TotalDurance.Value.HasValue)
+            {
+                DateTime d = (DateTime) TotalDurance.Value;
+                DateTime d2 = new DateTime(d.Year, d.Month, d.Day);
+                durance = d - d2;
+            }
+
+            Options = new DumpOptions()
+            {
+                Path = pathBox.Text,
+                Count = (useTotalCount.IsChecked ?? false) ? (TotalCnt.Value ?? int.MaxValue) : int.MaxValue,
+                Durance = (useTotalDurance.IsChecked ?? false) ? durance : TimeSpan.MaxValue,
+
+            };
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "new_capture";
+            dlg.DefaultExt = ".pcap";
+            dlg.Filter = "Libpcap capture file (.pcap)|*.pcap";
+
+            Nullable<bool> res = dlg.ShowDialog();
+
+            if (res == true)
+            {
+                pathBox.Text = dlg.FileName;
+            }
         }
     }
 }
